67229a3ddc00b81f42b6e182e072e45c1564b6cd
shohruz2502 <takekmsjan@gmail.com>
2025-11-29 19:57:44 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 const app = express();
 
